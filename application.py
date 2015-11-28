0a08e88565f2b8e709ca6be65c320002bf14527f
magneton_bora <dryndikroman@gmail.com>
2015-11-28 23:03:42 +0100

    Basic 2-node unconnected graph with cytoscape

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 
-from flask import Flask
+from flask import render_template, url_for, Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-	return "You're at home"
+	return render_template("index.html")
 
 
 if __name__ == '__main__':
